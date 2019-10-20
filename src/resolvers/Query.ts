7d4e2a8e4132dc1ea5b03503c5a911d9b7399abd
Ben Furber <ben@fu.rber.io>
2019-10-20 20:39:15 +0100

    ♻️ Tidy-up query to shrink lines

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -27,9 +27,7 @@ export const Query = {
   },
 
   async link(parent, { url }, ctx: Context) {
-    const where = { url };
-    const links = await ctx.prisma.links({ where });
-
+    const links = await ctx.prisma.links({ where: { url } });
     if (links.length > 0) return links[0];
 
     const createdLink = await link.createLink({}, { url }, ctx, {});
