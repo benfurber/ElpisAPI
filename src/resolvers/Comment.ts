b083d20b66b1335d97aea533b24fec2cf6f5b71f
Ben Furber <ben@fu.rber.io>
2020-03-06 18:00:11 +0000

    ✨ Add isAuthorCurrentUser to Comment query

--- src/resolvers/Comment.ts
+++ src/resolvers/Comment.ts
@@ -1,8 +1,14 @@
-import { Context } from "../utils";
+import { Context, getUserId } from "../utils";
 
 export const Comment = {
   author: ({ id }, args, ctx: Context) => {
     return ctx.prisma.comment({ id }).author();
+  },
+  isAuthorCurrentUser: async ({ id }, args, ctx: Context) => {
+    const userId = getUserId(ctx);
+    const author = await ctx.prisma.comment({ id }).author();
+
+    return userId === author.id;
   },
   replies: ({ id }, args, ctx: Context) => {
     return ctx.prisma.replies({ where: { comment: { id } } });
