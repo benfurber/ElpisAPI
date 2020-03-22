107828beef17e1bfdc47d6d288340627bfacb88e
Ben Furber <ben@fu.rber.io>
2020-03-22 13:33:08 +0000

    🗑 Drop posts from Users

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,9 +1,6 @@
 import { Context } from "../utils";
 
 export const User = {
-  posts: ({ id }, args, ctx: Context) => {
-    return ctx.prisma.user({ id }).posts();
-  },
   notifications: ({ id }, args, ctx: Context) => {
     return ctx.prisma.user({ id }).notifications({ orderBy: "createdAt_DESC" });
   },
